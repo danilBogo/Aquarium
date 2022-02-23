fc34259de24c6bb7ecdbe634dcd0b24f9a444fb8
Marat Elagin <maratelagin@gmail.com>
2022-02-23 21:49:16 +0300

    Created form

--- Aquarium/Aquarium/wwwroot/js/signalR.js
+++ Aquarium/Aquarium/wwwroot/js/signalR.js
@@ -1,19 +1,21 @@
-﻿const hubConnection = new signalR.HubConnectionBuilder()
-    .withUrl("/chat")
+﻿let connection = new signalR.HubConnectionBuilder()
+    .withUrl("/aquarium")
     .build();
-
-hubConnection.on("Send", function (data) {
-
-    let elem = document.createElement("p");
-    elem.appendChild(document.createTextNode(data));
-    let firstElem = document.getElementById("chatroom").firstChild;
-    document.getElementById ("chatroom").insertBefore(elem, firstElem);
-
-});
+connection.start()
 
 document.getElementById("sendBtn").addEventListener("click", function (e) {
-    let message = document.getElementById("message").value;
-    hubConnection.invoke("Send", message);
+    addFish();
 });
 
-hubConnection.start();
+function addFish() {
+    let fishId = document.getElementById("fishId");
+}
+
+
+connection.on("send", data => {
+    console.log(data);
+});
+
+    .then(() => connection.invoke("send", "Hello"));
+
+
